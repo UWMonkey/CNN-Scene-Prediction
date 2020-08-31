92d9673ec2874401747ed9b8e469c5159a3bc16f
UWMonkey <43870879+UWMonkey@users.noreply.github.com>
2020-08-31 13:55:08 -0400

    Delete evaluate.py

--- evaluate.py
+++
@@ -1,5 +0,0 @@
-def evaluate():
-    with tf.Graph().as_default():
-
-        log_dir = "C://Users//KG//Desktop//MMAI_894//Project//save_workspace//"
-        test_dir = "C://Users//KG//Desktop//MMAI_894//Project//Images//"
